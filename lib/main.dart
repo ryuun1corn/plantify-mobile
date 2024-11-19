c39d2406c17da7770c13e5b57f1762116ee438a7
Yudayana Arif Prasojo <yuda.arif.prasojo@gmail.com>
2024-11-19 18:47:08 +0700

    feat: add login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:plantify_mobile/screens/menu.dart';
+import 'package:provider/provider.dart';
+import 'package:plantify_mobile/screens/login.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,15 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Plantify',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.green,
-        ).copyWith(secondary: Colors.greenAccent),
-        useMaterial3: true,
-      ),
-      home: MyHomePage(),
-    );
+    return Provider(
+        create: (_) {
+          CookieRequest request = CookieRequest();
+          return request;
+        },
+        child: MaterialApp(
+          title: 'Plantify',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: Colors.green,
+            ).copyWith(secondary: Colors.greenAccent),
+            useMaterial3: true,
+          ),
+          home: const LoginPage(),
+        ));
   }
 }
